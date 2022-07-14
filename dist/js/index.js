a10b455e4fddfc941c45ca1844a922abd2727910
Italo Pedroza <74671728+italoknd@users.noreply.github.com>
2022-07-14 00:08:15 -0400

    tipando arrays

--- dist/js/index.js
+++ dist/js/index.js
@@ -1,3 +1,29 @@
 "use strict";
 let numero = 20;
-console.log("número >>>", numero);
+console.log('número >>>', numero);
+//tipos básicos
+let nome = 'Italo';
+let age = 22;
+let isAdmin = true;
+//tipando array
+let lista1 = [1, 2, 3, 4, 5]; //tipagem para array de números
+let lista2 = ['A', 'B', 'C']; //tipagem para array de strings
+let lista3 = [
+    {
+        nome: 'Italo',
+        idade: 23
+    },
+    {
+        nome: 'random',
+        idade: 9999999
+    }
+]; //tipando um array de objetos
+let list4 = [
+    1,
+    'Italo',
+    true,
+    2,
+    { nome: 'Italo', idade: 23 },
+    'Pedroza'
+]; //tipando uma tupla
+//definição de tupla: uma sequência ordenada e finita de elementos
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,32 @@
-let numero:number =  20
+let numero: number = 20
 
-console.log("número >>>", numero);
+console.log('número >>>', numero)
+
+//tipos básicos
+let nome: string = 'Italo'
+let age: number = 22
+let isAdmin: boolean = true
+
+//tipando array
+let lista1: number[] = [1, 2, 3, 4, 5] //tipagem para array de números
+let lista2: string[] = ['A', 'B', 'C'] //tipagem para array de strings
+let lista3: object[] = [
+  {
+    nome: 'Italo',
+    idade: 23
+  },
+  {
+    nome: 'random',
+    idade: 9999999
+  }
+] //tipando um array de objetos
+let list4: [number, string, boolean, number, object, string] = [
+  1,
+  'Italo',
+  true,
+  2,
+  { nome: 'Italo', idade: 23 },
+  'Pedroza'
+] //tipando uma tupla
+
+//definição de tupla: uma sequência ordenada e finita de elementos
