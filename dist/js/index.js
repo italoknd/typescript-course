0b12504f4d67be0726b2b6acf9011996602b52a8
Italo Pedroza <74671728+italoknd@users.noreply.github.com>
2022-07-21 15:44:48 -0400

    continuando os estudos em ts

--- dist/js/index.js
+++ dist/js/index.js
@@ -27,3 +27,31 @@ let list4 = [
     'Pedroza'
 ]; //tipando uma tupla
 //definição de tupla: uma sequência ordenada e finita de elementos
+//tipando obj
+const data = {
+    nome: "Italo",
+    idade: 22,
+    sexo: 'M',
+    altura: 1.75
+};
+//union type: permite atribuir mais de um tipo a um variável
+let id = 10;
+let idade = '10';
+let id2 = '10';
+let idade2 = 10;
+//enum: permite setar valores que podem se repetir no decorrer do sistema para facilitar o acessso ao mesmos e deixando o código menos verboso
+var Size;
+(function (Size) {
+    Size["P"] = "Pequeno";
+    Size["M"] = "M\u00E9dio";
+    Size["G"] = "Grande";
+})(Size || (Size = {}));
+const camisa = {
+    nome: "Camisa gola V",
+    tamanho: Size.M
+};
+const cueca = {
+    nome: "Cueca Lupo",
+    tamanho: Size.G
+};
+console.log(camisa);
--- src/index.ts
+++ src/index.ts
@@ -30,3 +30,40 @@ let list4: [number, string, boolean, number, object, string] = [
 ] //tipando uma tupla
 
 //definição de tupla: uma sequência ordenada e finita de elementos
+
+//tipando obj
+const data: {nome: string, idade: number, sexo: string, altura: number} = {
+  nome: "Italo", 
+  idade: 22, 
+  sexo: 'M', 
+  altura: 1.75
+}
+
+//union type: permite atribuir mais de um tipo a um variável
+let id: number | string = 10
+let idade: number | string = '10'
+
+
+//type alias: permitir criar um tipo de variável para receber vários outro tipos nativos do ts
+type myType = number | string
+let id2: myType = '10'
+let idade2: myType = 10
+
+//enum: permite setar valores que podem se repetir no decorrer do sistema para facilitar o acessso ao mesmos e deixando o código menos verboso
+enum Size {
+  P = "Pequeno",
+  M = "Médio",
+  G = "Grande"
+}
+
+const camisa = {
+  nome: "Camisa gola V",
+  tamanho: Size.M
+} 
+
+const cueca = {
+  nome: "Cueca Lupo",
+  tamanho: Size.G
+}
+
+console.log(camisa);
